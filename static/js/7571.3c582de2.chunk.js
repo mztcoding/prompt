a5cbe5a92fb0bd014a4b0fed5d6ee4537e60736f
Muhammad Zakariya <mzakariya673@gmail.com>
2023-10-12 06:30:31 -0700

    ready

---
+++ static/js/7571.3c582de2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprompt_react=self.webpackChunkprompt_react||[]).push([[7571],{7571:function(e,t,r){r.r(t);var a=r(9439),n=r(2791),c=r(6871),u=r(7217),f=r(184);t.default=function(){var e=(0,u.aU)(),t=(0,a.Z)(e,1)[0],r=(0,u.aF)(),p=(0,a.Z)(r,1)[0];return(0,n.useEffect)((function(){t()}),[t]),(0,f.jsx)(f.Fragment,{children:p&&(0,f.jsx)(c.Fg,{to:"/auth/login",replace:!0})})}}}]);
+//# sourceMappingURL=7571.3c582de2.chunk.js.map
